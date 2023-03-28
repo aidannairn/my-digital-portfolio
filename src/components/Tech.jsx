c1fbdfc75d7b334a71a237829d1bfe8d88a59715
aidannairn <aidan.nairn95@gmail.com>
2023-03-29 12:39:27 +1300

    feat: Create floating polygons that showcase technologies I use.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+
+import { textVariant } from '../utils/motion'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from '../hoc'
+import { technologies } from "../constants"
+import { styles } from '../styles'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Some of the languages, libraries and frameworks I use</p>
+        <h2 className={styles.sectionHeadText}>Technologies.</h2>
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center gap-10 mt-20">
+        {technologies.map(technology => (
+          <div key={technology.name} className="w-28 h-28">
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, 'tech')
