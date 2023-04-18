bcc276126e68cdca9381d5c0dc9c2fdb1559993b
aidannairn <aidan.nairn95@gmail.com>
2023-04-19 11:41:33 +1200

    feat: Delete a learning experience from the database. Remove logo and certificate images from S3 if present.

--- server/controllers/education.controller.js
+++ server/controllers/education.controller.js
@@ -1,4 +1,4 @@
-const { s3Upload } = require('../config/aws.config')
+const { s3Upload, s3Delete } = require('../config/aws.config')
 const Education = require('../models/Education')
 
 const educationCreate = async (req, res, next) => {
@@ -46,4 +46,32 @@ const educationCreate = async (req, res, next) => {
   }
 }
 
-module.exports = { educationCreate }
+const educationDeleteOne = async (req, res) => {
+  const { id: eduId } = req.params
+
+  // TODO: Get user ID and validate that the learning experience to be deleted belongs to the active user.
+
+  try {
+    const education = await Education.findByIdAndDelete(eduId)
+    if (!education) throw new Error('There was a problem removing this learning experience from the database.')
+
+    const { logoURL, certificateURL } = education
+    
+    if (logoURL) {
+      const logo = await s3Delete(logoURL)
+      if (!logo) throw new Error('Logo was not removed from S3.')
+    }
+
+    if (certificateURL) {
+      const certificate = await s3Delete(certificateURL)
+      if (!certificate) throw new Error('Certificate was not removed from S3.')
+    }
+
+    return res.status(200).json({ type: 'success', msg: 'Learning experience was removed successfully.' })
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({ type: 'error', msg: 'Could not delete this learning experience.' })
+  }
+}
+
+module.exports = { educationCreate, educationDeleteOne }
