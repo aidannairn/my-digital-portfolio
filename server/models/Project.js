7ae119ff7b560e1094b9fbe380113be3a081d6c8
aidannairn <aidan.nairn95@gmail.com>
2023-04-23 13:41:22 +1200

    feat: Update Project model and controller to include mandatory Project Description.

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ProjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   imageURL: { type: String, required: true },
+  description: { type: String, required: true },
   links: [{ title: String, url: String }],
   tags: [{ type: String, color: String }],
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
