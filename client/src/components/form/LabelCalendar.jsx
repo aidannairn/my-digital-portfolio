dbe5ee8985393f86e3ef40f19c9a220da5b8c4d6
aidannairn <aidan.nairn95@gmail.com>
2023-05-03 10:21:44 +1200

    refactor: Move calendar input fields into their own component.

--- client/src/components/form/LabelCalendar.jsx
+++ client/src/components/form/LabelCalendar.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react'
 
+import DateInputField from './DateInputField'
+
 const LabelCalendar = ({
   label,
   name,
-  value,
   required,
   handleChange,
   untilNow
@@ -24,37 +25,12 @@ const LabelCalendar = ({
 
   useEffect(() => { setCurrentDate(new Date()) }, [])
 
-  const handleDateChange = e => {
-    const { name, min, max } = e.target
-    let { value } = e.target
-    const limit = name === 'year' ? 4 : 2
-    value = value.slice(0, limit)
-    if (max && value.length === limit && +value > +max) value = max
-    if (min && value.length === limit && +value < +min) value = min
-    setDate({ ...date, [name]: value })
-  }
-
-  const handleInputKeyUp = e => {
-    const { name, value, max } = e.target
-    const digitsBeforeNext = name === 'year' ? null : 2
-
-    if (digitsBeforeNext) {
-      const currInputRef = inputRefMap.findIndex(ref =>
-        ref.current === e.target)
-      
-      if (value.length === digitsBeforeNext && value <= max) 
-        inputRefMap[currInputRef + 1].current.focus()
-    }
-  }
-
   const populateDaysInMonth = newDate => {
     const month = newDate.toLocaleString('default', { month: 'long' })
-
-    let totalDays = 0
-
     const days30 = ['January', 'March', 'May', 'July', 'August', 'October', 'December']
     const days31 = ['April', 'June', 'September', 'November']
-
+    let totalDays = 0
+    
     if (days30.includes(month)) {
       totalDays = 30
     } else if (days31.includes(month)) {
@@ -65,14 +41,6 @@ const LabelCalendar = ({
       totalDays = isLeapYear ? 29 : 28
     }
     return totalDays
-  }
-
-  const handleInputBlur = e => {
-    let { name, value } = e.target
-    if (name === 'year' && value.length < 4) return
-    
-    value = value.padStart(2, '0')
-    setDate({ ...date, [name]: value })
   }
 
   useEffect(() => {
@@ -94,50 +62,39 @@ const LabelCalendar = ({
     }
   }, [date])
   
-
   return (
     <div className='mb-6'>
       <span className='text-white font-medium mb-4'>{label}{required ? <i className='font-thin'> (required)</i> : ''}</span>
       <div className='mt-2 flex gap-3'>
         <label className='flex items-center w-full'>
-          <input
+          <DateInputField
             ref={dayInputRef}
-            type='number'
-            className='bg-tertiary py-4 px-2 sm:w-[5rem] w-full text-center placeholder:text-secondary text-white rounded-l-lg outlined-none border-none font-medium'
-            placeholder='dd'
             name='day'
+            placeholder='dd'
             min='1'
             max={daysInMonth}
-            value={date.day}
-            onChange={handleDateChange}
-            onKeyUp={handleInputKeyUp}
-            onBlur={handleInputBlur}
+            inputRefMap={inputRefMap}
+            date={{ state: date, setState: setDate }}
           />
           <span className='bg-tertiary py-4 pr-[0.25rem] sm:pr-4'>-</span>
-          <input
+          <DateInputField
             ref={monthInputRef}
-            type='number'
-            className='bg-tertiary py-4 px-2 sm:w-[5rem] w-full text-center placeholder:text-secondary text-white outlined-none border-none font-medium'
-            placeholder='mm'
             name='month'
+            placeholder='mm'
             min='1'
             max='12'
-            value={date.month}
-            onChange={handleDateChange}
-            onKeyUp={handleInputKeyUp}
-            onBlur={handleInputBlur}
+            inputRefMap={inputRefMap}
+            date={{ state: date, setState: setDate }}
           />
           <span className='bg-tertiary py-4 pr-[0.25rem] sm:pr-4'>-</span>
-          <input
+          <DateInputField
             ref={yearInputRef}
-            type='number'
-            className='bg-tertiary py-4 px-2 sm:w-[5rem] w-full text-center placeholder:text-secondary text-white rounded-r-lg outlined-none border-none font-medium'
-            placeholder='yyyy'
             name='year'
+            placeholder='yyyy'
             min={currentDate.getFullYear() - 100}
             max={currentDate.getFullYear() + (untilNow ? 0 : 100)}
-            value={date.year}
-            onChange={handleDateChange}
+            inputRefMap={inputRefMap}
+            date={{ state: date, setState: setDate }}
           />
         </label>
       </div>
