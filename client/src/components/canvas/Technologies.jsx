6d8e020ed8342728d1e2b1b6ac42fe661afc34f7
aidannairn <aidan.nairn95@gmail.com>
2023-05-10 12:09:04 +1200

    refactor: Move *Card components from "Experiences", "Projects" and "Technologies" into their own files.

--- client/src/components/canvas/Technologies.jsx
+++ client/src/components/canvas/Technologies.jsx
@@ -1,88 +1,10 @@
-import { forwardRef, useEffect, useRef, useState, Suspense } from 'react'
+import { useRef, useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Decal, Float, useTexture, OrthographicCamera, Html, Preload } from '@react-three/drei'
+import { OrthographicCamera, Preload } from '@react-three/drei'
 
 import { OnConfirmModal, TechDetailModal } from '../modals'
 import CanvasLoader from '../Loader'
-
-const TechItem = forwardRef(({
-  gridDimensions: gd,
-  index,
-  handleTechItemClick,
-  imageURL,
-  position,
-  scale
-}, ref) => {
-  /*
-    - The default behavior of floatingRange is for objects to have less range the closer they are to the center.
-    - setFloatingRange is designed to make each object have the same amount of floatingRange regardless of their position.
-  */
-  const getFloatingRange = () => {
-    // Set the maximum distance from the center.
-    const maxDist= Math.ceil(gd.x + gd.y)
-    
-    // Get object's distance from center.
-    const distFromCenter = position.reduce((prev, curr) =>
-      prev + Math.abs(curr), 0)
-
-    /* 
-      Divide maxDist by distFromCenter.
-      - This value will be used to reduce the amount of floating range the further they are positioned from the center.
-      - The further from the center; The lower the value will be.
-      - If the object has a position of [0, 0, 0], zero will divide into maxDist "Infinity" times.
-    */
-    const maxDivideDFC = maxDist / distFromCenter
-
-    const rangeVal = maxDivideDFC !== Infinity ? maxDivideDFC * 0.01 : 0.1
-
-    const floatRange = [-rangeVal, rangeVal]
-    return floatRange
-  }
-
-  const [ decal ] = useTexture([ imageURL ])
-  const [isHovered, setIsHovered] = useState(false)
-  const [floatRange, setFloatRange] = useState(0)
-
-  useEffect(() => { setFloatRange(getFloatingRange) }, [])
-  useEffect(() => {
-    if (ref.current)
-      ref.current.style.cursor = isHovered ? 'pointer' : 'default'
-  }, [isHovered])
-
-  return (
-    <Float 
-      speed={isHovered ? 0 : (Math.random() * (1.8 - 1.7) + 1.7).toFixed(2)}
-      rotationIntensity={1}
-      floatIntensity={1}
-      floatingRange={floatRange} // default [-0.1, 0.1]
-    >
-      <mesh
-        castShadow
-        receiveShadow
-        position={position}
-        scale={isHovered ? 1.25 * scale : scale}
-        onClick={() => handleTechItemClick(index)}
-        onPointerEnter={() => setIsHovered(true)}
-        onPointerLeave={() => setIsHovered(false)}
-      >
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial 
-          color='#FFF8EB'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[ 2 * Math.PI, 0, 6.25 ]}
-          scale={1.25}
-          flatShading
-          map={decal}
-        />
-      </mesh>
-    </Float>
-  )
-})
+import TechnologyCard from './TechnologyCard'
 
 const TechCanvas = ({
   technologies,
@@ -176,7 +98,7 @@ const TechCanvas = ({
         <Suspense fallback={<CanvasLoader />}>
           { positions.length === technologies.length &&
             technologies.map((technology, i) => (
-              <TechItem
+              <TechnologyCard
                 ref={canvasRef}
                 key={`tech-${technology._id}`}
                 index={i}
---
+++ client/src/components/canvas/TechnologyCard.jsx
@@ -0,0 +1,83 @@
+import { forwardRef, useEffect, useState } from 'react'
+import { Decal, Float, useTexture } from '@react-three/drei'
+
+const TechnologyCard = forwardRef(({
+  gridDimensions: gd,
+  index,
+  handleTechItemClick,
+  imageURL,
+  position,
+  scale
+}, ref) => {
+  /*
+    - The default behavior of floatingRange is for objects to have less range the closer they are to the center.
+    - setFloatingRange is designed to make each object have the same amount of floatingRange regardless of their position.
+  */
+  const getFloatingRange = () => {
+    // Set the maximum distance from the center.
+    const maxDist= Math.ceil(gd.x + gd.y)
+    
+    // Get object's distance from center.
+    const distFromCenter = position.reduce((prev, curr) =>
+      prev + Math.abs(curr), 0)
+
+    /* 
+      Divide maxDist by distFromCenter.
+      - This value will be used to reduce the amount of floating range the further they are positioned from the center.
+      - The further from the center; The lower the value will be.
+      - If the object has a position of [0, 0, 0], zero will divide into maxDist "Infinity" times.
+    */
+    const maxDivideDFC = maxDist / distFromCenter
+
+    const rangeVal = maxDivideDFC !== Infinity ? maxDivideDFC * 0.01 : 0.1
+
+    const floatRange = [-rangeVal, rangeVal]
+    return floatRange
+  }
+
+  const [ decal ] = useTexture([ imageURL ])
+  const [isHovered, setIsHovered] = useState(false)
+  const [floatRange, setFloatRange] = useState(0)
+
+  useEffect(() => { setFloatRange(getFloatingRange) }, [])
+  useEffect(() => {
+    if (ref.current)
+      ref.current.style.cursor = isHovered ? 'pointer' : 'default'
+  }, [isHovered])
+
+  return (
+    <Float 
+      speed={isHovered ? 0 : (Math.random() * (1.8 - 1.7) + 1.7).toFixed(2)}
+      rotationIntensity={1}
+      floatIntensity={1}
+      floatingRange={floatRange} // default [-0.1, 0.1]
+    >
+      <mesh
+        castShadow
+        receiveShadow
+        position={position}
+        scale={isHovered ? 1.25 * scale : scale}
+        onClick={() => handleTechItemClick(index)}
+        onPointerEnter={() => setIsHovered(true)}
+        onPointerLeave={() => setIsHovered(false)}
+      >
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial 
+          color='#FFF8EB'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[ 2 * Math.PI, 0, 6.25 ]}
+          scale={1.25}
+          flatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  )
+})
+
+export default TechnologyCard
