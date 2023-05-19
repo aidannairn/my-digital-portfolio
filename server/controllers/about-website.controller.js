ae04b49e26e26828071735c677b9ed9d8fa53e84
aidannairn <aidan.nairn95@gmail.com>
2023-05-20 09:55:15 +1200

    feat: Complete webDemoCreate function. Website demo block can be saved to the database.

--- server/controllers/about-website.controller.js
+++ server/controllers/about-website.controller.js
@@ -1,8 +1,53 @@
+const { s3Upload, s3Delete } = require('../config/aws.config')
+const WebDemo = require('../models/WebsiteDemo')
+const StatusCodeError = require('../utils/statusCodeError')
+
 const webDemoCreate = async (req, res) => {
+  const { title, subtitle, bullets: bulletsStr, duration, } = req.body
+  const userId = req.userId
+  const image = req.file
+  
   try {
-    
+    if (!userId) throw new StatusCodeError(401, 'The user is not authenticated to make this request.')
+
+    if (!(title && bullets.length && image ))
+    throw new StatusCodeError(400, 'A new website demo block should include a title, some key points about the website and an image')
+
+    var demoURL = image
+      ? await s3Upload(image.originalname, image.path, 'website_demo')
+      : undefined
+
+    const bullets = bulletsStr
+      ? await JSON.parse(bulletsStr)
+      : undefined
+
+    const webDemo = new WebDemo({
+      title,
+      subtitle,
+      bullets,
+      demoURL,
+      duration,
+      userId
+    })
+
+    await webDemo.save()
+    return res.status(200).json({
+      alert: {
+        type: 'success',
+        msg: `${title} demo block has been added!`
+      },
+      webDemo
+    })
+
   } catch (error) {
-    
+    if (demoURL) s3Delete(demoURL)
+    console.error(error)
+    return res.status(error?.errorCode || 400).json({
+      alert: {
+        type: 'error',
+        msg: error?.msg || 'Could not add new demo block.'
+      }
+    })
   }
 }
 
--- server/models/WebsiteDemo.js
+++ server/models/WebsiteDemo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const WebsiteDemoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subtitle: String,
-  demo: { type: String, required: true },
+  demoURL: { type: String, required: true },
   bullets: [{ type: String, required: true }],
   duration: Number,
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
